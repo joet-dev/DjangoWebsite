09ee513bf3519ef97131fc01a3196318d2cf5934
Joseph-Thur <69287038+Zenokue@users.noreply.github.com>
2023-01-12 15:19:12 +1000

    Detail page tweaked. Setup item description

--- DjangoWebsite/portfoliosite/portfolio/models.py
+++ DjangoWebsite/portfoliosite/portfolio/models.py
@@ -9,6 +9,7 @@ class PortfolioItem(models.Model):
     item_date = models.DateTimeField('date published')
     item_hits = models.IntegerField(default=0)
     item_img_id = models.CharField(max_length=150)
+    # item_details = models.TextField('details')
 
     def __str__(self):
         return self.item_title
