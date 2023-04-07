176a84033e697827df8283c438f9647450687842
Joseph-Thur <69287038+Zenokue@users.noreply.github.com>
2023-04-07 20:15:49 +1000

    Updated how navigation bar links work.

--- DjangoWebsite/portfoliosite/portfolio/models.py
+++ DjangoWebsite/portfoliosite/portfolio/models.py
@@ -8,7 +8,9 @@ class PortfolioItem(models.Model):
     item_title = models.CharField(max_length=200)
     item_date = models.DateTimeField('date published')
     item_hits = models.IntegerField(default=0)
+    item_summary = models.TextField('summary', default="There is no summary currently. Sorry!")
     item_img_id = models.CharField(max_length=150)
+    item_git_link = models.CharField(max_length=100, null=True)
     item_details = models.TextField('details', default="There are no details currently. Sorry!")
 
     def __str__(self):
--- DjangoWebsite/portfoliosite/portfolio/urls.py
+++ DjangoWebsite/portfoliosite/portfolio/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import path, re_path
+from django.views.generic.base import RedirectView
 from . import views
 
+# favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
 urlpatterns = [
+    # re_path(r'^favicon\.ico$', favicon_view),
     path('', views.index, name='index'),
     path('<int:item_id>/', views.detail, name='detail'),
 ]
